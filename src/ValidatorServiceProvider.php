78f8d56f76ffc0e7edbedbea0fc5808bbf0d1d9c
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-10-26 23:16:46 +0200

    Initial commit

---
+++ src/ValidatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SebastiaanLuca\Validator;
+
+use Illuminate\Support\ServiceProvider;
+
+class ValidatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
